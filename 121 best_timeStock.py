cc6e9036fc9268167cf145a399ed0cd855abac29
Aditya Chavan <130660675+Aditya-A-Chavan@users.noreply.github.com>
2024-07-17 18:05:54 +0530

    solved 121 w better algo

--- 121 best_timeStock.py
+++ 121 best_timeStock.py
@@ -1,23 +1,19 @@
-# You are given an array prices where prices[i] is the price of a given stock on the ith day.
-
-# # You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-# #
-# # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
-
-
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        if not prices:
-            return 0
-        
-        min_price = prices[0]
-        max_profit = 0
+    def maxProfit(self, prices: list[int]) -> int:
+        left = 0
+        right = 1
+        maxprofit = 0
 
-        for price in prices:
-            if price < min_price:
-                min_price = price
-            elif price-min_price > max_profit:
-                max_profit = price-min_price
-        
-        return max_profit
-        
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxprofit = max(maxprofit, profit)
+            else:
+                left = right
+            right += 1 
+
+        return maxprofit
+
+
+prices = [7,1,5,3,6,4]
+print(Solution().maxProfit(prices))
